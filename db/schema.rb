58975e205b53cfc3e76efc26ba9ecb6751c6455a
Ariel <poonsaucey@comcast.net>
2015-07-29 18:31:30 -0400

    landlord test fixed / passes #hooray

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729213155) do
+ActiveRecord::Schema.define(version: 20150729221950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "landlords", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "name"
     t.string   "email"
+    t.string   "password_digest"
   end
 
   create_table "tenants", force: :cascade do |t|
