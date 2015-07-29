355056ca7968e13a912eaa204427b1ef201cbe48
Sean Sellek <me@seansellek.com>
2015-07-29 18:32:27 -0400

    Added Properties table

---
+++ spec/models/property_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Property, type: :model do
+  it {should respond_to(:name)}
+  it "has address properties" do
+    subject.should respond_to(:address1)
+    subject.should respond_to(:address2)
+    subject.should respond_to(:city)
+    subject.should respond_to(:state)
+    subject.should respond_to(:zip)
+  it
+
+end
