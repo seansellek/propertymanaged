64d0742c33dd04ca8165d354e91962a6357170c6
Ariel <poonsaucey@comcast.net>
2015-07-30 19:58:16 -0400

    re enabled validations

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -2,15 +2,9 @@ class Landlord < ActiveRecord::Base
   has_many :properties
   has_secure_password
 
-  # validates :email, presence: true
-  # validates :name, presence: true
-  # validates :password, confirmation: true, :length => {minimum: 8, message: 'password is too short'}
-  # validates :password_confirmation, presence: true
-  # validates_uniqueness_of :email, :case_sensitive => false
-  #
-  #
-  #
-  #
-  #
-  # validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :name, presence: true
+  validates :password, confirmation: true, :length => {minimum: 8, message: 'password is too short'}
+  validates :password_confirmation, presence: true
+  validates :email, format: { :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/ }
 end
