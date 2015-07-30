6dfa6f7a1b1db2fa94f9d811eb0157b89e4b7a2a
Ariel <poonsaucey@comcast.net>
2015-07-30 16:42:22 -0400

    made landlord tests of password validity and email uniqueness pass

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -1,6 +1,16 @@
 class Landlord < ActiveRecord::Base
   has_many :properties
-  has_secure_password 
+  has_secure_password
+
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :password, confirmation: true, :length => {minimum: 8, message: 'password is too short'}
+  validates :password_confirmation, presence: true
+  validates_uniqueness_of :email, :case_sensitive => false
+
+
+
+
 
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 end
