a10619e50d52280e1b8f379574e948a6a2a7aee1
Sean Sellek <me@seansellek.com>
2015-07-31 01:58:36 -0400

    Completed Landlord Create/Update actions

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -9,9 +9,27 @@ class LandlordsController < ApplicationController
     if @landlord.save
       flash[:notice] = "The Landlord is successfully saved!"
       redirect_to signup_path #, notice: "Created Landlord"
+    else
+      flash[:error] = @landlord.errors.full_messages[0]
+      redirect_to signup_path
     end
   end
 
+  def edit
+    @landlord = Landlord.find params[:id]
+  end
+
+  def update
+    @landlord = Landlord.find params[:id]
+
+    if @landlord.update_attributes user_params
+      flash[:notice] = 'The Landlord is successfully updated!'
+      redirect_to edit_landlord_path
+    else
+      flash[:error] = @landlord.errors.full_messages[0]
+      redirect_to edit_landlord_path
+    end
+  end
   private
   def user_params
     params.require(:landlord).permit(:name, :password, :password_confirmation, :email)
--- spec/features/landlords_spec.rb
+++ spec/features/landlords_spec.rb
@@ -37,17 +37,77 @@ RSpec.describe "Landlord Feature Tests" do
   end
 
   context 'POST /landlord' do
-    it 'creates and saves the valid landlord' do
+    before do
       visit new_landlord_path
-
       fill_in 'Email', with: 'xajler@gmail.com'
-      fill_in 'Password', with: 'x1234567'
-      fill_in 'Confirm Password', with: 'x1234567'
+      fill_in 'Password', with: 'z1234567'
+      fill_in 'Confirm Password', with: 'z1234567'
       fill_in 'Name', with: 'Kornelije Sajler'
+    end
+    it 'creates and saves the valid landlord' do
       click_button 'Sign Up'
 
-      current_path.should be == signup_path
       page.should have_content 'The Landlord is successfully saved!'
+    end
+    context "doesn't save invalid landlord" do
+      it 'when passwords mismatch' do
+        fill_in 'Confirm Password', with: 'x1234567'
+        click_button 'Sign Up'
+
+        page.should have_content "Password confirmation doesn't match"
+      end
+      it 'when email is blank' do
+        fill_in 'Email', with: ''
+        click_button 'Sign Up'
+
+        page.should have_content "Email can't be blank"
+      end
+      it 'when password is blank' do
+        fill_in 'Password', with: ''
+        click_button 'Sign Up'
+
+        page.should have_content "Password can't be blank"
+      end
+      it 'when email is not unique' do
+        create :landlord
+
+        click_button 'Sign Up'
+
+        page.should have_content "Email has already been taken"
+      end
+      it 'when password is less than 8 characters' do
+        fill_in 'Password', with: '123'
+        fill_in 'Confirm Password', with: '123'
+        click_button 'Sign Up'
+
+        page.should have_content "Password is too short (minimum is 8 characters)"
+      end
+    end
+  end
+  context 'PUT landlords/id' do
+    it 'valid landlord update' do
+      landlord = create :landlord
+      visit edit_landlord_path landlord
+
+      find_field('Email').value.should be == 'xajler@gmail.com'
+      find_field('Name').value.should be == 'Kornelije Sajler'
+
+      fill_in 'Password', with: 'aoeuidht'
+      fill_in 'Confirm Password', with: 'aoeuidht'
+      fill_in 'Name', with: 'Kornelije Sajler - xajler'
+      click_button 'Update Landlord'
+
+      page.should have_content 'Landlord is successfully updated!'
+    end
+    it 'invalid when passwords mismatch' do
+      landlord = create :landlord
+      visit edit_landlord_path landlord
+
+      fill_in 'Password', with: 'aoeuidht'
+      fill_in 'Confirm Password', with: 'aoeu'
+      click_button 'Update Landlord'
+
+      page.should have_content "Password confirmation doesn't match"
     end
   end
 end
