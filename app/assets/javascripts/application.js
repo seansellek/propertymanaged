ef0ad2d9dbd911e1c4202df36b92b92bf099a430
Sean Sellek <me@seansellek.com>
2015-08-08 17:46:33 -0400

    Switched to javascript =(

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,57 +1,47 @@
-require 'turbolinks'
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
 
-require_tree '.'
+var ready;
+ready = function() {
+    $('.properties').on('click', '.invite-tenant', displayer);
+    $('#popup-form').on('click', '#invite-close', formHide);
+    $('#popup-form').on('submit', formSubmit);
+};
 
+$(document).ready(ready);
+$(document).on('page:load', ready);
 
-class Timeout
-def initialize(time = 0, & block)
-`setTimeout(function(){#{block.call}}, time)`
-end
-end
+function displayer(event) {
+    event.preventDefault();
+    var path = $(event.target).attr('href');
 
+    $.ajax(path, {
+        success: function(response) {
+            var form = response;
+            $('#popup-form').html(form).slideDown();
+        }
+    });
+};
 
-hide_error = Proc.new do
-    Element.find('#error').css('margin-top', '-50px')
-end
-displayer = Proc.new do |
-event |
-    event.prevent
-path = event.target.attr(: href)
-HTTP.get(path) do |
-request |
-    form = request.body
-puts form
-Element.find('#popup-form').html(form).slide_down
-end
-end
-form_hide = Proc.new do |
-event |
-    event.prevent
-Element.find('#popup-form').slide_up
-end
-form_submit = Proc.new do |
-event |
-    popup = Element.find('#popup-form')
-popup.slide_up
-puts 'Yey'
-end
-monetize = Proc.new do |
-event |
-    amount = event.target.value
-digits = amount.to_s.split
-digits.include
+function formHide(event) {
+    event.preventDefault;
+    $('#popup-form').slideUp();
+};
 
-end
-
-
-
-# #
-# Invite Tenant Form
-# #
-Document.ready ? do
-    Element.find('.properties').on(: click, '.invite-tenant', & displayer)
-Element.find('#popup-form').on(: click, '#invite-close', & form_hide)
-Element.find('#popup-form').on(: submit, & form_submit)
-Element.find('#popup-form').on(: input, '#invite_amount', & monetize)
-end
-Timeout.new(5000, & hide_error)
+function formSubmit(event) {
+    $('#popup-form').slideUp();
+};
